011e0e8ac8a159b0135d0d3cc36d680fee439a29
Daniel Borguezani <daniel_borguezani@hotmail.com>
2026-02-04 21:43:23 -0300

    feat: atualiza configuração da baseURL no axios para usar variável de ambiente

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { enqueueSnackbar } from 'notistack';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
 });
 
 axiosInstance.interceptors.request.use(
